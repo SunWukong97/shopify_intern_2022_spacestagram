d50f929abcbd2effa225282edb80a1d628dde353
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-09-13 19:52:18 -0400

    created the react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let todayDate = new Date().toISOString().slice(0, 10);
+  let startDate = new Date();
+  startDate.setDate(startDate.getDate() - 10);
+  startDate = startDate.toISOString().slice(0, 10);
+  const [isLoading, setIsLoading] = useState(true);
+  const [jsonData, setJsonData] = useState(null);
+  // console.log(todayDate);
+
+  //console.log(startDate);
+
+  let test;
+  let apiKey = "vn0fMtp6NRQpMEMQlXgvuFttofKGuxcMKFG0cixN";
+  let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${startDate}&end_date=${todayDate}`;
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("no reposnse");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setJsonData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("problem fetching data");
+      });
+  }, []);
+
+  function arrayLoop(data2) {
+    let info = [];
+    for (let item of data2) {
+      console.log(item.title);
+      info.push(
+        <Card
+          title={item.title}
+          imageUrl={item.url}
+          date={item.date}
+          description={item.explanation}
+        />
+      );
+    }
+    return info;
+  }
+  if (!isLoading) {
+    test = arrayLoop(jsonData);
+    console.log(jsonData);
+  }
+  return <div>{test}</div>;
 }
 
 export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Card(props) {
+  const { title, imageUrl, date, description } = props;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{date}</p>
+      <img src={imageUrl} alt="an picture taken by NASA of space" />
+      <p>{description}</p>
+    </div>
+  );
+}
+export default Card;
