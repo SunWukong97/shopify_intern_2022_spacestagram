e3041b796881e9c1a89dd359e04a61f5ab252749
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-09-15 10:24:32 -0400

    Added heart icon from font awesome
    now when clicking the like button a heart icon will appear to indicate the post has been liked. Will reverse back to the word like if the post has been unliked.

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -3,27 +3,38 @@ import "./LikeButton.css";
 function LikeButton() {
   const [clickedLike, setClickedLike] = useState(false);
 
-  if (clickedLike) {
-    return (
-      <button
-        className="my-like-button"
-        onClick={handleClick}
-        style={{ backgroundColor: "green" }}
-      >
-        Like
-      </button>
-    );
-  } else {
-    return (
-      <button
-        className="my-like-button"
-        onClick={handleClick}
-        style={{ backgroundColour: "white" }}
-      >
-        Like
-      </button>
-    );
-  }
+  let icon = clickedLike ? <i class="fas fa-heart"></i> : "Like";
+  let backgroundColour = clickedLike ? "red" : "white";
+  return (
+    <button
+      className="my-like-button"
+      onClick={handleClick}
+      style={{ backgroundColor: backgroundColour }}
+    >
+      {icon}
+    </button>
+  );
+  // if (clickedLike) {
+  //   return (
+  //     <button
+  //       className="my-like-button"
+  //       onClick={handleClick}
+  //       style={{ backgroundColor: "green" }}
+  //     >
+  //       <i class="fas fa-heart"></i>
+  //     </button>
+  //   );
+  // } else {
+  //   return (
+  //     <button
+  //       className="my-like-button"
+  //       onClick={handleClick}
+  //       style={{ backgroundColour: "white" }}
+  //     >
+  //       Like
+  //     </button>
+  //   );
+  // }
 
   function handleClick() {
     setClickedLike(!clickedLike);
