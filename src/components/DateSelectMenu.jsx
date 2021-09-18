12c34c24c50aa66dde9c53ba16ab2aa99860efae
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-09-18 14:05:17 -0400

    Create DateSelectMenu.jsx
    created the component to allow users to select how far back they want to see the posts up to 2 months.

---
+++ src/components/DateSelectMenu.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function DateSelectMenu(props) {
+  const { dateSelection } = props;
+
+  const values = [10, 20, 30, 60];
+
+  function handleChange(event) {
+    dateSelection(event.target.value);
+  }
+
+  return (
+    <div className="date-selector-menu">
+      <label htmlFor="date-selector">Posts from last</label>
+      <select name="date-selector" id="date-selector" onChange={handleChange}>
+        <option value={values[0]}>10 days</option>
+        <option value={values[1]}>20 days</option>
+        <option value={values[2]}>30 days</option>
+        <option value={values[3]}>2 months</option>
+      </select>
+    </div>
+  );
+}
+export default DateSelectMenu;
